854a54702c5ac90c5efcad3c3c1e17608ea23a92
Aleksandar Buljevic <abuljevic8@gmail.com>
2021-11-14 20:17:02 +0100

    Fixed issue with security manager

--- src/main/java/akatsuki/restaurantsysteminformation/configurations/WebSecurityConfig.java
+++ src/main/java/akatsuki/restaurantsysteminformation/configurations/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import akatsuki.restaurantsysteminformation.security.JWTUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
