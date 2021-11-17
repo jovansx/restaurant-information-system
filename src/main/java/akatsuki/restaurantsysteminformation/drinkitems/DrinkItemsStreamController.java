a7fd04c795e6610969a9c69b6d5baeed78f706a5
jovansx <jovansimic995@gmail.com>
2021-11-17 21:50:50 +0100

    Small changes socket.

--- src/main/java/akatsuki/restaurantsysteminformation/drinkitems/DrinkItemsStreamController.java
+++ src/main/java/akatsuki/restaurantsysteminformation/drinkitems/DrinkItemsStreamController.java
@@ -1,18 +1,34 @@
 package akatsuki.restaurantsysteminformation.drinkitems;
 
+import akatsuki.restaurantsysteminformation.drinkitems.dto.DrinkItemsActionRequestDTO;
+import akatsuki.restaurantsysteminformation.drinkitems.dto.ItemsActiveDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 public class DrinkItemsStreamController {
 
-    @MessageMapping({"/hello"})
-    @SendTo("/topic/greetings")
-    public String greetings(String message) throws InterruptedException {
-        Thread.sleep(1000);
-        return "Hello";
+    private final DrinkItemsService drinkItemsService;
+
+//    @MessageMapping({"/hello"})
+//    @SendTo("/topic/drink-items")
+//    public String greetings(String message) throws InterruptedException {
+//        Thread.sleep(1000);
+//        return "Hello";
+//    }
+
+    @MessageMapping({"/drink-items/change-state"})
+    @SendTo("/topic/drink-items")
+    public ItemsActiveDTO changeStateOfDrinkItems(@RequestBody @Valid DrinkItemsActionRequestDTO dto) {
+        return new ItemsActiveDTO(drinkItemsService.changeStateOfDrinkItems(dto.getItemId(), dto.getUserId()));
     }
 }
