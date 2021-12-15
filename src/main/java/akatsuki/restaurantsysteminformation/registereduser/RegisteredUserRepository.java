6a000a3245fb38e76114a8ffc067d6f305a6342a
jovansx <jovansimic995@gmail.com>
2021-12-15 15:43:20 +0100

    Small refactoring at RegisteredUserServiceTest.

--- src/main/java/akatsuki/restaurantsysteminformation/registereduser/RegisteredUserRepository.java
+++ src/main/java/akatsuki/restaurantsysteminformation/registereduser/RegisteredUserRepository.java
@@ -1,6 +1,5 @@
 package akatsuki.restaurantsysteminformation.registereduser;
 
-import akatsuki.restaurantsysteminformation.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,6 +11,6 @@ import java.util.Optional;
 public interface RegisteredUserRepository extends JpaRepository<RegisteredUser, Long> {
     Optional<RegisteredUser> findByUsername(String username);
 
-    @Query("select u from User u where u.type = 2")
-    List<User> findAllSystemAdmins();
+    @Query("select u from RegisteredUser u where u.type = 2")
+    List<RegisteredUser> findAllSystemAdmins();
 }
