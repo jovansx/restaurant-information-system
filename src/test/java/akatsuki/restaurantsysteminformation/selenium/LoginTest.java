f1ce097853c8f421d4a5751a98264d34111606ef
jovansx <jovansimic995@gmail.com>
2021-12-25 12:17:06 +0100

    E2e testing in progress...

--- src/test/java/akatsuki/restaurantsysteminformation/selenium/LoginTest.java
+++ src/test/java/akatsuki/restaurantsysteminformation/selenium/LoginTest.java
@@ -1,21 +1,21 @@
 package akatsuki.restaurantsysteminformation.selenium;
 
 import akatsuki.restaurantsysteminformation.seleniumpages.LoginPage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import akatsuki.restaurantsysteminformation.seleniumpages.SystemAdminWorkersPage;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LoginTest {
 
-    private WebDriver browser;
-    private LoginPage loginPage;
+    private static WebDriver browser;
+    private static LoginPage loginPage;
+    private static SystemAdminWorkersPage systemPage;
 
-    @BeforeEach
-    public void setup() {
+    @BeforeAll
+    public static void setup() {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
 
         browser = new ChromeDriver();
@@ -23,10 +23,12 @@ public class LoginTest {
         browser.navigate().to("http://localhost:4200/login");
 
         loginPage = PageFactory.initElements(browser, LoginPage.class);
+        systemPage = PageFactory.initElements(browser, SystemAdminWorkersPage.class);
 
     }
 
     @Test
+    @Order(1)
     public void badCredentials() {
         loginPage.ensureIsDisplayedLoginBtn();
         Assertions.assertEquals(browser.getTitle(), "RestaurantInformationSystemFrontend");
@@ -41,6 +43,7 @@ public class LoginTest {
     }
 
     @Test
+    @Order(2)
     public void successfulLogin() {
         loginPage.ensureIsDisplayedLoginBtn();
         Assertions.assertEquals(browser.getTitle(), "RestaurantInformationSystemFrontend");
@@ -54,8 +57,26 @@ public class LoginTest {
         Assertions.assertEquals("http://localhost:4200/home/system-admin/workers", browser.getCurrentUrl());
     }
 
-    @AfterEach
-    public void tearDown() {
-        browser.quit();
+    @Test
+    public void successfulEditing() {
+        Assertions.assertEquals(9, systemPage.getTableRows().size());
+        Assertions.assertFalse(systemPage.getFirstNameInput().isEnabled());
+        Assertions.assertEquals("John", systemPage.getFirstNameInput().getAttribute("value"));
+
+        systemPage.clickSelectedRow(1);
+        Assertions.assertEquals("Simon", systemPage.getFirstNameInput().getAttribute("value"));
+
+        systemPage.clickButton(0);
+        Assertions.assertTrue(systemPage.getFirstNameInput().isEnabled());
+        systemPage.setFirstNameInput("Pera zdera");
+        Assertions.assertEquals("Pera zdera", systemPage.getFirstNameInput().getAttribute("value"));
+
+        systemPage.clickButton(1);
+        Assertions.assertFalse(systemPage.getFirstNameInput().isEnabled());
+    }
+
+    @AfterAll
+    public static void tearDown() {
+//        browser.quit();
     }
 }
---
+++ src/test/java/akatsuki/restaurantsysteminformation/seleniumpages/SystemAdminWorkersPage.java
@@ -0,0 +1,50 @@
+package akatsuki.restaurantsysteminformation.seleniumpages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SystemAdminWorkersPage {
+
+    private WebDriver driver;
+
+    @FindBy(xpath = "//table[1]/tbody/tr")
+    private List<WebElement> tableRows;
+
+    @FindBy(xpath = "//form/mat-form-field[1]/div/div[1]/div/input")
+    private WebElement firstNameInput;
+
+    @FindBy(xpath = "/html/body/app-root/app-workers/div/mat-card/form/div/button")    // Cancel -> Edit -> Save (3)
+    private List<WebElement> buttons;
+
+    public SystemAdminWorkersPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void setFirstNameInput(String firstName) {
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+    }
+
+    public void clickSelectedRow(int index) {
+        WebElement row = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(tableRows.get(index)));
+        row.click();
+    }
+
+    public void clickButton(int index) {
+        WebElement button = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(buttons.get(index)));
+        button.click();
+    }
+}
