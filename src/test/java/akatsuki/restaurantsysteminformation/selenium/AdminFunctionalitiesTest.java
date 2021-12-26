e3a1c28f2e82764de1bdc162c83d24172e8d1d6b
jovansx <jovansimic995@gmail.com>
2021-12-26 11:17:29 +0100

    Added 2 tests for editing -> admin.

--- src/test/java/akatsuki/restaurantsysteminformation/selenium/AdminFunctionalitiesTest.java
+++ src/test/java/akatsuki/restaurantsysteminformation/selenium/AdminFunctionalitiesTest.java
@@ -1,8 +1,10 @@
 package akatsuki.restaurantsysteminformation.selenium;
 
+import akatsuki.restaurantsysteminformation.seleniumpages.AdminAdministratorsPage;
 import akatsuki.restaurantsysteminformation.seleniumpages.LoginPage;
 import akatsuki.restaurantsysteminformation.seleniumpages.ManagerEmployeesPage;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -14,9 +16,9 @@ public class AdminFunctionalitiesTest {
     private static WebDriver browser;
 
     private static LoginPage loginPage;
-//    private static ManagerEmployeesPage managerPage;
+    private static AdminAdministratorsPage adminPage;
 
-//    private final String MAT_FORM_FIELD_TO_INPUT = "div/div[1]/div/input";
+    private final String MAT_FORM_FIELD_TO_INPUT = "div/div[1]/div/input";
 
     @BeforeAll
     public static void setup() {
@@ -27,7 +29,7 @@ public class AdminFunctionalitiesTest {
         browser.navigate().to("http://localhost:4200/login");
 
         loginPage = PageFactory.initElements(browser, LoginPage.class);
-//        managerPage = PageFactory.initElements(browser, ManagerEmployeesPage.class);
+        adminPage = PageFactory.initElements(browser, AdminAdministratorsPage.class);
 
     }
 
@@ -61,6 +63,64 @@ public class AdminFunctionalitiesTest {
         Assertions.assertEquals("http://localhost:4200/home/admin/restaurant-view", browser.getCurrentUrl());
 
         browser.navigate().to("http://localhost:4200/home/admin/administrators");
+    }
+
+    @Test
+    public void successfulEditing() throws InterruptedException {
+        Assertions.assertEquals(1, adminPage.getTableRows().size());
+        Assertions.assertFalse(adminPage.getFirstNameInput().isEnabled()); // Check inputs are disabled
+        Assertions.assertFalse(adminPage.getPhoneNumberInput().isEnabled());
+        Assertions.assertEquals("Liam", adminPage.getFirstNameInput().getAttribute("value"));  // Check inputs values set to initially selected row
+        Assertions.assertEquals("0611111116", adminPage.getPhoneNumberInput().getAttribute("value"));
+
+        adminPage.clickButton(0);  // Enable editing
+        Assertions.assertTrue(adminPage.getFirstNameInput().isEnabled());  // Check inputs are enabled
+        Assertions.assertTrue(adminPage.getPhoneNumberInput().isEnabled());
+        adminPage.setFirstNameInput("Liamor"); // Enter new values
+        adminPage.setPhoneNumber("0645558888");
+        Assertions.assertEquals("Liamor", adminPage.getFirstNameInput().getAttribute("value"));    // Check if new values are set to inputs
+        Assertions.assertEquals("0645558888", adminPage.getPhoneNumberInput().getAttribute("value"));
+
+        adminPage.clickButton(1);  // Click on save button
+        Thread.sleep(1000);
+        Assertions.assertFalse(adminPage.getFirstNameInput().isEnabled()); // Check inputs are disabled
+        Assertions.assertFalse(adminPage.getPhoneNumberInput().isEnabled());
+        Assertions.assertEquals("Liamor Neeson", adminPage.getTableRows().get(0).findElement(By.xpath("td[2]")).getText()); // Check if new values are updated in the table
+        Assertions.assertEquals("0645558888", adminPage.getTableRows().get(0).findElement(By.xpath("td[3]")).getText());
+
+        adminPage.clickButton(0);
+        adminPage.setFirstNameInput("Liam");
+        adminPage.setPhoneNumber("0611111116");
+        adminPage.clickButton(1);
+        Thread.sleep(1000);
+        Assertions.assertEquals("Liam Neeson", adminPage.getTableRows().get(0).findElement(By.xpath("td[2]")).getText());
+        Assertions.assertEquals("0611111116", adminPage.getTableRows().get(0).findElement(By.xpath("td[3]")).getText());
+    }
+
+    @Test
+    public void failEditing_BadFirstName() {
+        Assertions.assertEquals(1, adminPage.getTableRows().size());
+        Assertions.assertFalse(adminPage.getFirstNameInput().isEnabled()); // Check inputs are disabled
+        Assertions.assertFalse(adminPage.getPhoneNumberInput().isEnabled());
+        Assertions.assertEquals("Liam", adminPage.getFirstNameInput().getAttribute("value"));  // Check inputs values set to initially selected row
+        Assertions.assertEquals("0611111116", adminPage.getPhoneNumberInput().getAttribute("value"));
+
+        adminPage.getButtons().get(0).click();  // Enable editing
+        Assertions.assertTrue(adminPage.getFirstNameInput().isEnabled());  // Check inputs are enabled
+        Assertions.assertTrue(adminPage.getPhoneNumberInput().isEnabled());
+        adminPage.setPhoneNumber("06933skl"); // Enter new value for phone number
+        Assertions.assertEquals("06933skl", adminPage.getPhoneNumberInput().getAttribute("value"));    // Check if new value is set to input
+
+        adminPage.clickButton(1);  // Click on save button
+        Assertions.assertTrue(adminPage.getFirstNameInput().isEnabled()); // Check inputs are enabled, save should not be triggered if inputs are not valid
+        Assertions.assertTrue(adminPage.getPhoneNumberInput().isEnabled());
+        Assertions.assertEquals("0611111116", adminPage.getTableRows().get(0).findElement(By.xpath("td[3]")).getText()); // Check if new value is not saved
+
+        adminPage.clickButton(0);  // Click on cancel button
+        Assertions.assertFalse(adminPage.getFirstNameInput().isEnabled()); // Check inputs are disabled
+        Assertions.assertFalse(adminPage.getPhoneNumberInput().isEnabled());
+        Assertions.assertEquals("Liam Neeson", adminPage.getTableRows().get(0).findElement(By.xpath("td[2]")).getText());
+        Assertions.assertEquals("0611111116", adminPage.getTableRows().get(0).findElement(By.xpath("td[3]")).getText());
     }
 
     @AfterAll
