f8b2315bb598732db42a4f5e770bcff5a9db7d8d
Aleksandar Buljevic <abuljevic8@gmail.com>
2021-12-27 18:17:42 +0100

    Merged, but need to be refactored

--- src/test/java/akatsuki/restaurantsysteminformation/selenium/AdminFunctionalitiesTest.java
+++ src/test/java/akatsuki/restaurantsysteminformation/selenium/AdminFunctionalitiesTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,22 +19,20 @@ public class AdminFunctionalitiesTest {
     private static WebDriver browser;
 
     private static LoginPage loginPage;
-    private static AdminAdministratorsPage adminSystemAdministratorsPage;
-    private static AdminRestaurantViewPage adminRestaurantViewPage;
+    private static AdminAdministratorsPage adminPage;
+    private static AdminRestaurantViewPage restaurantPage;
 
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
 
-        browser = new ChromeDriver(options);
+        browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.navigate().to("http://localhost:4200/login");
 
         loginPage = PageFactory.initElements(browser, LoginPage.class);
-        adminSystemAdministratorsPage = PageFactory.initElements(browser, AdminAdministratorsPage.class);
-        adminRestaurantViewPage = PageFactory.initElements(browser, AdminRestaurantViewPage.class);
+        adminPage = PageFactory.initElements(browser, AdminAdministratorsPage.class);
+        restaurantPage = PageFactory.initElements(browser, AdminRestaurantViewPage.class);
     }
 
     @Test
@@ -62,6 +59,7 @@ public class AdminFunctionalitiesTest {
     }
 
     @Test
+    @Order(4)
     public void successfulEditing() {
         assertEquals(1, adminPage.getTableRows(1).size());
         Utilities.elementDisabledWait(browser, adminPage.getFirstNameInput(), 10);
@@ -99,6 +97,7 @@ public class AdminFunctionalitiesTest {
     }
 
     @Test
+    @Order(3)
     public void failEditing_BadFirstName() {
         assertEquals(1, adminPage.getTableRows(1).size());
         Utilities.elementDisabledWait(browser, adminPage.getFirstNameInput(), 10);
@@ -133,6 +132,7 @@ public class AdminFunctionalitiesTest {
     }
 
     @Test
+    @Order(5)
     public void successfulAddingAndDeletingOfSystemAdmin() {
         adminPage.clickAddButton();
         adminPage.setFields("Lucian", "List", "lucian@gmail.com", "lucian_stronger",
@@ -152,8 +152,248 @@ public class AdminFunctionalitiesTest {
 
     }
 
-    @AfterAll
-    public static void tearDown() {
-        browser.quit();
+    @Test
+    @Order(6)
+    public void checkingIfButtonsAreDisabled() {
+        browser.navigate().to("http://localhost:4200/home/admin/restaurant-view");
+
+        Assertions.assertTrue(restaurantPage.addRoomButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.renameRoomButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.deleteRoomButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.editRoomButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.applyLayoutButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.saveButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.cancelButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.rowInput.isEnabled());
+        Assertions.assertFalse(restaurantPage.colInput.isEnabled());
     }
+
+    @Test
+    @Order(7)
+    public void failingAddingRoom_RoomNameIsAlreadyUsed() {
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        restaurantPage.addRoomButton.click();
+        restaurantPage.ensureDialogIsOpen();
+        restaurantPage.dialogNameInput.sendKeys("Room 1");
+        restaurantPage.dialogAddButton.click();
+        restaurantPage.ensureDialogIsClosed();
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+    }
+
+
+    @Test
+    @Order(8)
+    public void successfullyAddingRoom_RoomIsAdded() {
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        restaurantPage.addRoomButton.click();
+        restaurantPage.ensureDialogIsOpen();
+        restaurantPage.dialogNameInput.sendKeys("Room 3");
+        restaurantPage.dialogAddButton.click();
+        restaurantPage.ensureDialogIsClosed();
+        Assertions.assertEquals(3, restaurantPage.rooms.size());
+        Assertions.assertEquals("Room 3", restaurantPage.rooms.get(2).getText());
+    }
+
+    @Test
+    @Order(9)
+    public void successfullyDeletingRoom_RoomDeleted() {
+        Assertions.assertEquals(3, restaurantPage.rooms.size());
+        restaurantPage.secondTab.click();
+        restaurantPage.clickButtonUntilItIsClicked(restaurantPage.deleteRoomButton);
+        restaurantPage.ensureRoomIsDeleted();
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+    }
+
+    @Test
+    @Order(10)
+    public void failingRenameRoom_RoomNamAlreadyExist() {
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        restaurantPage.secondTab.click();
+        restaurantPage.clickButtonUntilItIsClicked(restaurantPage.renameRoomButton);
+        restaurantPage.ensureDialogIsOpen();
+        restaurantPage.dialogNameInput.clear();
+        restaurantPage.dialogNameInput.sendKeys("Room 1");
+        restaurantPage.dialogAddButton.click();
+        restaurantPage.ensureDialogIsClosed();
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        Assertions.assertEquals("Room 3", restaurantPage.rooms.get(1).getText());
+    }
+
+    @Test
+    @Order(11)
+    public void successfullyRenameRoom_RoomNameIsUpdated() {
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        restaurantPage.clickButtonUntilItIsClicked(restaurantPage.renameRoomButton);
+        restaurantPage.ensureDialogIsOpen();
+        restaurantPage.dialogNameInput.clear();
+        restaurantPage.dialogNameInput.sendKeys("Room 2");
+        restaurantPage.dialogAddButton.click();
+        restaurantPage.ensureDialogIsClosed();
+        Assertions.assertEquals(2, restaurantPage.rooms.size());
+        Assertions.assertEquals("Room 2", restaurantPage.rooms.get(1).getText());
+    }
+
+    @Test
+    @Order(12)
+    public void successfullyEnablingAndCancelingEditMode_EditModeEnabledAndCanceled() {
+        Assertions.assertFalse(restaurantPage.applyLayoutButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.saveButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.cancelButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.colInput.isEnabled());
+        Assertions.assertFalse(restaurantPage.rowInput.isEnabled());
+        Assertions.assertTrue(restaurantPage.editRoomButton.isEnabled());
+
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+
+        Assertions.assertTrue(restaurantPage.saveButton.isEnabled());
+        Assertions.assertTrue(restaurantPage.cancelButton.isEnabled());
+        Assertions.assertTrue(restaurantPage.colInput.isEnabled());
+        Assertions.assertTrue(restaurantPage.rowInput.isEnabled());
+        Assertions.assertFalse(restaurantPage.editRoomButton.isEnabled());
+
+        restaurantPage.cancelButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.editRoomButton);
+
+        Assertions.assertFalse(restaurantPage.saveButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.cancelButton.isEnabled());
+        Assertions.assertFalse(restaurantPage.colInput.isEnabled());
+        Assertions.assertFalse(restaurantPage.rowInput.isEnabled());
+        Assertions.assertTrue(restaurantPage.editRoomButton.isEnabled());
+    }
+
+    @Test
+    @Order(13)
+    public void successfullyEnablingAndAddingTableAndCancelingEditMode_EditModeEnabledAndCanceled() {
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+        restaurantPage.buttonWithCordinates00.click();
+        restaurantPage.ensureTableDialogIsShown();
+
+        Assertions.assertEquals("Empty", restaurantPage.tableStateInput.getText());
+
+        restaurantPage.tableStateInput.click();
+        restaurantPage.ensureTableStateSelectIsShown();
+
+        restaurantPage.circleButton.click();
+
+        restaurantPage.tableNameInput.clear();
+        restaurantPage.tableNameInput.sendKeys("T1");
+
+        restaurantPage.tableDialogOkButton.click();
+        restaurantPage.ensureNumberOfTablesIs(1);
+        Assertions.assertEquals(1, restaurantPage.tables.size());
+
+
+        restaurantPage.cancelButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.editRoomButton);
+        Assertions.assertEquals(0, restaurantPage.tables.size());
+    }
+
+    @Test
+    @Order(14)
+    public void successfullyAddingTable_TableAddedToRoom() {
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+        restaurantPage.buttonWithCordinates00.click();
+        restaurantPage.ensureTableDialogIsShown();
+
+        Assertions.assertEquals("Empty", restaurantPage.tableStateInput.getText());
+
+        restaurantPage.tableStateInput.click();
+        restaurantPage.ensureTableStateSelectIsShown();
+
+        restaurantPage.circleButton.click();
+
+        restaurantPage.tableNameInput.clear();
+        restaurantPage.tableNameInput.sendKeys("T1");
+
+        restaurantPage.tableDialogOkButton.click();
+        restaurantPage.ensureNumberOfTablesIs(1);
+        Assertions.assertEquals(1, restaurantPage.tables.size());
+
+
+        restaurantPage.saveButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.editRoomButton);
+        Assertions.assertEquals(1, restaurantPage.tables.size());
+        Assertions.assertEquals("T1", restaurantPage.getNameOfTable(restaurantPage.tables.get(0)));
+    }
+
+    @Test
+    @Order(15)
+    public void successfullyEditingTable_TableUpdated() {
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+        restaurantPage.buttonWithCordinates00.click();
+        restaurantPage.ensureTableDialogIsShown();
+
+        Assertions.assertEquals("Circle", restaurantPage.tableStateInput.getText());
+
+        restaurantPage.tableStateInput.click();
+        restaurantPage.ensureTableStateSelectIsShown();
+
+        restaurantPage.squareButton.click();
+
+        restaurantPage.tableNameInput.clear();
+        restaurantPage.tableNameInput.sendKeys("T2");
+
+        restaurantPage.tableDialogOkButton.click();
+        restaurantPage.ensureNumberOfTablesIs(1);
+        Assertions.assertEquals(1, restaurantPage.tables.size());
+
+
+        restaurantPage.clickButtonUntilItIsClicked(restaurantPage.saveButton);
+        Assertions.assertEquals(1, restaurantPage.tables.size());
+        Assertions.assertEquals("T2", restaurantPage.getNameOfTable(restaurantPage.tables.get(0)));
+    }
+
+    @Test
+    @Order(16)
+    public void successfullyRemovingTable_TableDeleted() {
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+        restaurantPage.buttonWithCordinates00.click();
+        restaurantPage.ensureTableDialogIsShown();
+
+        Assertions.assertEquals("Square", restaurantPage.tableStateInput.getText());
+
+        restaurantPage.tableStateInput.click();
+        restaurantPage.ensureTableStateSelectIsShown();
+
+        restaurantPage.emptyButton.click();
+
+        restaurantPage.tableDialogOkButton.click();
+        restaurantPage.ensureNumberOfTablesIs(0);
+        Assertions.assertEquals(0, restaurantPage.tables.size());
+
+
+        restaurantPage.saveButton.click();
+        Assertions.assertEquals(0, restaurantPage.tables.size());
+    }
+
+    @Test
+    @Order(17)
+    public void successfullyChangingLayoutOfTable_TableLayoutUpdated() {
+        restaurantPage.editRoomButton.click();
+        restaurantPage.ensureButtonIsEnabled(restaurantPage.saveButton);
+
+        restaurantPage.rowInput.clear();
+        restaurantPage.rowInput.sendKeys("3");
+        restaurantPage.colInput.clear();
+        restaurantPage.colInput.sendKeys("3");
+
+        restaurantPage.applyLayoutButton.click();
+        restaurantPage.ensureTableLayoutIsUpdated();
+
+        restaurantPage.saveButton.click();
+
+        Assertions.assertTrue(restaurantPage.buttonWithCordinates02.isDisplayed());
+    }
+
+
 }
