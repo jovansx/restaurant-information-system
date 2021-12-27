a37c8ecf4a5bc81869ddc812ab911b6323ddde5a
jovansx <jovansimic995@gmail.com>
2021-12-27 11:15:06 +0100

    Refactored manager functionalities.

--- src/test/java/akatsuki/restaurantsysteminformation/seleniumpages/Utilities.java
+++ src/test/java/akatsuki/restaurantsysteminformation/seleniumpages/Utilities.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,12 +21,28 @@ public class Utilities {
         return new WebDriverWait(driver, Duration.ofSeconds(wait)).until(ExpectedConditions.titleIs(title));
     }
 
+    public static void elementDisabledWait(WebDriver driver, WebElement element, int wait) {
+        new WebDriverWait(driver, Duration.ofSeconds(wait)).until((ExpectedCondition<Boolean>) condition -> !element.isEnabled());
+    }
+
+    public static void elementEnabledWait(WebDriver driver, WebElement element, int wait) {
+        new WebDriverWait(driver, Duration.ofSeconds(wait)).until((ExpectedCondition<Boolean>) condition -> element.isEnabled());
+    }
+
     public static WebElement visibilityWait(WebDriver driver, WebElement element, int wait) {
         return new WebDriverWait(driver, Duration.ofSeconds(wait)).until(ExpectedConditions.visibilityOf(element));
     }
 
     public static List<WebElement> visibilityWait(WebDriver driver, By locator, int wait) {
         return new WebDriverWait(driver, Duration.ofSeconds(wait)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    }
+
+    public static List<WebElement> numberOfElementsWait(WebDriver driver, By locator, int number, int wait) {
+        return new WebDriverWait(driver, Duration.ofSeconds(wait)).until(ExpectedConditions.numberOfElementsToBe(locator, number));
+    }
+
+    public static void numberOfElementsWait(WebDriver driver, List<WebElement> elements, int number, int wait) {
+        new WebDriverWait(driver, Duration.ofSeconds(wait)).until((ExpectedCondition<Boolean>) condition -> elements.size() == number);
     }
 
     public static WebElement presenceWait(WebDriver driver, By locator, int wait) {
